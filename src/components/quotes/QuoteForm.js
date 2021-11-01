69903fb32ad4162d0778885beeb307ecdaa6e627
Jethro Williams <jethrowilliams@outlook.com>
2021-11-01 17:51:32 +0000

    Use the <Prompt> component to warn users if they try to navigate away from the form page after they start entering data

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,15 @@
-import { useRef } from 'react';
+import { useRef, useState, Fragment } from 'react';
+import { Prompt } from 'react-router-dom';
+// 'Prompt' will watch if we navigate away
+// Then, if a certain condition is met, it will show a warning before it allows us to leave
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
-const QuoteForm = (props) => {
+const QuoteForm = props => {
+  const [isEntering, setIsEntering] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,28 +24,52 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishEnteringHandler = () => {
+    setIsEntering(false); // We can't set this within 'submitFormHandler' because the state would be updated too late
+    // We would trigger the navigation action before the 'isEntering' state got updated
+    // However, the 'onClick' on the button gets triggered before we handle the form submission
+  };
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formFocusedHandler = () => {
+    setIsEntering(true);
+  };
+
+  return (
+    <Fragment>
+      <Prompt
+        when={isEntering}
+        message={location => `Do you want to redirect to ${location.pathname}`}
+      />
+      {/* 'Prompt' requires two props */}
+      {/* 'when' should be true/false, defining if the warning should be shown when the user changes the URL or not */}
+      {/* 'message' should be a function that returns a string, of the text that we show to the user when they try to leave */}
+      {/* It is a function, because we get the 'location' object as a parameter, which holds some information about the page the user is trying to go to (although we're not using the location here) */}
+      {/* It wasn't too easy to figure-out the content of 'location', but the 'location' object keys are 'pathname', 'search' , 'hash', 'state' and 'key' */}
+      {/* The only one that looked useful to me was 'pathname'. This could be used as something like 'message={location => `Do you want to redirect to the ${location.pathname} page?`}' */}
+      <Card>
+        <form onFocus={formFocusedHandler} className={classes.form} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input type='text' id='author' ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishEnteringHandler} className='btn'>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
