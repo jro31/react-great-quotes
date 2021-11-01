7cb4fbda03c674ddecaa7ab3b98c02d13ae1a22b
Jethro Williams <jethrowilliams@outlook.com>
2021-11-01 17:12:34 +0000

    Add 'Programatic Navigation' (re-routing the user automatically) using the 'useHistory()' hook
    Includes an explanation of the difference between the 'useHistory()' 'push' method, and the 'replace' method.

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,18 @@
+import { useHistory } from 'react-router';
+// 'useHistory' is named as such because it allows us to edit the browser history (the history of pages that we've visited')
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+	const history = useHistory(); // Returns a history object
+
 	const addQuoteHandler = quoteData => {
 		console.log(quoteData);
+
+		history.push('/quotes');
+		// 'history.push' pushes a new page onto our stack of pages, so essentially adds a new page to our history
+		// 'history.replace' replaces the current page, much like a redirect
+		// The difference is that with 'push' we can go 'back' (with the back button) to the current page. With 'replace', we can't.
 	};
 
 	return <QuoteForm onAddQuote={addQuoteHandler} />;
