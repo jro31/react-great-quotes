0653d648a94fc7e2e7046c1c3a2a8e88b119d4af
Jethro Williams <jethrowilliams@outlook.com>
2021-11-01 16:33:47 +0000

    Add a link to a dynamic URL using the <Link> component, and handle if this dynamic URL doesn't exist (for example if a user manually enters an ID that doesn't exist)
    And copy some dummy data

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,17 +1,27 @@
 import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+	{ id: 'q1', author: 'Harry', text: 'Expecto Patronum' },
+	{ id: 'q2', author: 'Voldermort', text: 'Avada Kedavra' },
+];
 
 const QuoteDetail = () => {
 	const params = useParams();
 
+	const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+	if (!quote) {
+		return <p>No quote found</p>;
+	}
+
 	return (
 		<Fragment>
-			<h1>Quote Detail Page</h1>
-			<p>{params.quoteId}</p>
+			<HighlightedQuote text={quote.text} author={quote.author} />
 			<Route path={`/quotes/${params.quoteId}/comments`}>
-				{/* As this is a route (and not a link) path='/quotes/:quoteId/comments' would also be ok */}
 				<Comments />
 			</Route>
 		</Fragment>
