b5bc098a0d051644156ca1c7818c5af2c2e37e69
Jethro Williams <jethrowilliams@outlook.com>
2021-11-02 13:24:05 +0000

    Add the 'comments' feature to complete the app
    Everything in this commit has been covered in previous commits in this or other repos

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,56 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { quoteId } = params;
+  // Object destructuring
+  // We extract the 'quoteId' from the params here, so that 'quoteId' can be used as a dependency in 'useEffect()' (below)
+  // (otherwise, passing-in 'params' as a dependency, the effect would be re-run anytime any param changed)
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+  // We use 'useCallback()' here because this function is used as a dependency in 'useEffect()' in the 'NewCommentForm.js' file
+  // Without 'useCallback()' here, this function would re-render anytime the component re-renders, and that would create an infinite loop
+  // (as this component would re-render, causing this function would re-render, causing the 'useEffect()' to re-render, causing this component to re-render, causing this function to re-render etc.)
+
+  let comments;
+
+  if (status === 'pending') {
+    comments = (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className='centered'>No comments were added yet!</p>;
+  }
 
   return (
     <section className={classes.comments}>
@@ -18,8 +60,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={addedCommentHandler} />}
+      {comments}
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,20 +1,42 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './NewCommentForm.module.css';
 
 const NewCommentForm = props => {
   const commentTextRef = useRef();
 
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
+
   const submitFormHandler = event => {
     event.preventDefault();
 
+    const enteredText = commentTextRef.current.value;
+
     // optional: Could validate here
 
-    // send comment to server
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
+    // We get the quote ID from props, rather than using 'useParams()' here, because it means we can reuse this component in places that don't include the quote ID in the params
+    // If that restriction doesn't matter (so if this component will only ever be used in a place that the quote ID is available in the params), then using 'useParams()' is fine
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
